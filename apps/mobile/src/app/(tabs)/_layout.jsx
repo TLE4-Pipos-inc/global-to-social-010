1845d994510560ebe9e7a779864f6f015852c3d2
Bryan_1025561 <1025561@hr.nl>
2026-06-03 11:35:58 +0200

    feat: fixed header duplicate code

--- apps/mobile/src/app/(tabs)/_layout.jsx
+++ apps/mobile/src/app/(tabs)/_layout.jsx
@@ -10,11 +10,8 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors.tint,
-        headerShown: false,
         tabBarButton: HapticTab,
-
         headerShown: true,
-
         headerTitleStyle: {
           fontSize: 32,
           fontWeight: "700",
